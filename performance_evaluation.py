442f6a5dc91dd2cdfb428997888f5964f2077e71
moquem <maena.quemener@yahoo.com>
2022-11-29 23:14:04 +0100

    Evaluation perf

--- performance_evaluation.py
+++ performance_evaluation.py
@@ -5,27 +5,27 @@ from random_newick_tree import random_newick_tree
 from minimal_crossing import OTCM
 
 
-N_MAX = 10000
+N_MAX = 11
 N_AVG = 100
-X = np.array(range(N_MAX))
+X = np.array([int(np.sqrt(10)**i) for i in range(N_MAX)])
 
 avg_time = np.zeros(N_MAX)
 worst_time = np.zeros(N_MAX)
 avg_conflict = np.zeros(N_MAX)
 
-for i in range(1,N_MAX):
-    print("Executing on tree of size",i)
+for i in range(N_MAX):
+    print("Executing on tree of size",int(np.sqrt(10)**i))
     avg_t = 0
     avg_c = 0
     worst_t = 0
-    for j in range(N_AVG):
-        nT = random_newick_tree(i)
+    for j in range(1,N_AVG):
+        nT = random_newick_tree(int(np.sqrt(10)**i))
         start = time.time()
         m,_ = OTCM(nT)
         end = time.time()
         worst_t = max(worst_t,end-start)
-        avg_t = (avg_t*(i-1)+(end-start))/i
-        avg_c = (avg_c*(i-1)+m)/i
+        avg_t = (avg_t*(j-1)+(end-start))/j
+        avg_c = (avg_c*(j-1)+m)/j
     avg_time[i] = avg_t
     worst_time[i] = worst_t
     avg_conflict[i] = avg_c
@@ -33,19 +33,19 @@ for i in range(1,N_MAX):
 plt.subplot(211)
 plt.yscale('log')
 plt.xscale('log')
-plt.bar(X,avg_time)
+plt.scatter(X,avg_time,s=2)
 plt.ylabel("Time of execution")
 plt.scatter(X,worst_time,color='orange',s=2)
 plt.plot()
 
 
 plt.subplot(212)
-plt.bar(X,avg_conflict)
+plt.plot(X,avg_conflict)
 plt.xlabel("Size of the newick tree")
 plt.ylabel("Number of conflicts")
 plt.plot()
 
-plt.show()
+plt.savefig('perfomance_evaluation_2.png')
 
 
     
